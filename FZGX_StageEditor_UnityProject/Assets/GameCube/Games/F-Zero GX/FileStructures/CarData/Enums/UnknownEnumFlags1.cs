38f71fb5f05d872948d4307dbd35852de6a61c28
Raphael Tetreault <raphaeltetreault@hotmail.com>
2018-08-03 00:49:26 -0400

    Properly set enum flag values to unique indices

--- FZGX_StageEditor_UnityProject/Assets/GameCube/Games/F-Zero GX/FileStructures/CarData/Enums/UnknownEnumFlags1.cs
+++ FZGX_StageEditor_UnityProject/Assets/GameCube/Games/F-Zero GX/FileStructures/CarData/Enums/UnknownEnumFlags1.cs
@@ -2,13 +2,13 @@
 {
     public enum UnknownEnumFlags1
     {
-        Bit0_001,
-        Bit1_DriftCamera,
-        Bit2_004,
-        Bit3_008,
-        Bit4_016,
-        Bit5_032,
-        Bit6_064,
-        Bit7_128,
+        Bit0_001 = 1 << 0,
+        Bit1_DriftCamera = 1 << 1,
+        Bit2_004 = 1 << 2,
+        Bit3_008 = 1 << 3,
+        Bit4_016 = 1 << 4,
+        Bit5_032 = 1 << 5,
+        Bit6_064 = 1 << 6,
+        Bit7_128 = 1 << 7,
     }
 }
